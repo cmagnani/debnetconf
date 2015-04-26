0ae558e4b95d1979ef004f8bad3991b415e8d1e4
Christophe MAGNANI <christophe@magnani.fr>
2015-04-26 23:57:43 +0200

    Creation de nouveaux ecran

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # debnetconf
 Reconfigration du reseau
 
-Un petit prohet personel pour simplifier la configuration reseau d'une debian
+Un petit projet personel pour simplifier la configuration reseau d'une debian
 
 Le but est de faire une petite interface simple 
 
--- debnetconf.sh
+++ debnetconf.sh
@@ -5,7 +5,7 @@
 
 
 # Dialog avec paramètres par defaut
-DIALOG='/usr/bin/dialog'
+DIALOG='/usr/bin/dialog --backtitle DebNetConf'
 DIALOG_OUINON=$DIALOG' --no-label Non --yes-label Oui'
 DIALOG_SUIVPREC=$DIALOG' --cancel-label Suivant --no-label Suivant' 
 
@@ -16,26 +16,32 @@ SCREEN_SIZE='20 60'
 
 
 # Fonctions
-function ConfigDHCP {
+function NetConfigDHCP {
    echo  test2
    }
 
-function ConfigManuel {
+function NetConfigManuel {
+    $DIALOG  --title 'Parametres TCPIP' --form '\n' $SCREEN_SIZE 16 \
+                                    'Adresse :' 1 1 '' 1 25 25 30 \
+                                    'Masque de sous-reseau :' 2 1 '' 2 25 25 30 \
+                                    'Passerelle :' 3 1 '' 3 25 25 30 \
+                                    'Serveur DNS1 :' 4 1 '' 4 25 25 30 \
+                                    'Serveur DNS2 :' 5 1 '' 5 25 25 30 \
+                                     2> $TEMPFILE    
     echo hop
    }
 
-function ConfigMode {
+function NetConfigMode {
     $DIALOG --radiolist 'Mode reseau' $SCREEN_SIZE 15 1 DHCP ON 2 Manuel OFF \
         2> $TEMPFILE
     
     RETVAL=$?
     CHOIX=`cat $TEMPFILE`
 
-
     case $RETVAL in
         0) case $CHOIX in
-            1) MODE='DHCP';;
-            2) MODE='MANUEL';;
+            1) NetConfigDHCP;;
+            2) NetConfigManuel;;
             esac
             ;;
         1) echo 'Sotie par annuler'
@@ -45,6 +51,38 @@ function ConfigMode {
     esac
 }
 
+function GenClefSSH {
+    echo hop
+}
 
-ConfigMode
+function ChoixConfig {
+    $DIALOG --checklist 'Choix configuration' $SCREEN_SIZE 15 1 'Génération des clefs SSH' OFF 2 'Configuration réseau' OFF \
+        2> $TEMPFILE
+
+    RETVAL=$?
+    CHOIX=`cat $TEMPFILE`
+
+    case $RETVAL in
+        0) for ITEM in $CHOIX; do
+            echo $ITEM
+            if [ "$ITEM"="1" ]; then
+                GenClefSSH 
+            fi 
+            if [ "$ITEM"="2" ]; then
+                NetConfigMode
+            fi
+           done
+            
+            ;;
+        1) echo 'Sortie par annuler'
+            exit;;
+        255) echo 'Dortie par l utilisateur'
+            exit;;
+    esac
+
+}
+
+# Main 
+ChoixConfig
+# NetConfigMode
 echo $MODE
